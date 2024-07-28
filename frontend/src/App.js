2678e45387739015bc0f78a36fbc4a1ca28b6471
Jim Xie <jjx5@scarletmail.rutgers.edu>
2024-07-28 19:17:10 -0400

    Added the planned frontend pages.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Home from "./components/Home.js";
 import LoginPage from "./components/LoginPage.js";
 import RegisterPage from "./components/RegisterPage.js";
-import NavBar from "./components/NavBar.js"
+import NavBar from "./components/LoggedOutNavBar.js"
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 class App extends React.Component {
